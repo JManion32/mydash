0bb00c13df0f48cf7ce70fe06ec502052fc47d0c
JManion32 <jmanion32@gmail.com>
2026-05-05 21:40:45 -0400

    Added Prettier, ESLint, and Stylelint

---
+++ site/eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import vue from 'eslint-plugin-vue'
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...vue.configs['flat/recommended'],
+
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser
+      }
+    }
+  },
+
+  {
+    rules: {
+      // your preferences
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'vue/multi-word-component-names': 'off'
+    }
+  }
+]
